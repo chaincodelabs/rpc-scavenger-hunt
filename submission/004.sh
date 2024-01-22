4195fdfba5986dc605f985f3203a7136d0b85503
Matthew Zipkin <pinheadmz@gmail.com>
2024-01-22 10:17:26 -0500

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,2 @@
-# Using descriptors, compute the 100th taproot address derived from this extended public key:
+# Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
